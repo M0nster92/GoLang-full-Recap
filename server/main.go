375fdf66fab1675ccd4daae7a0319d22e87f80ab
Sami2077 <fahimalsafi@gmail.com>
2021-01-10 17:47:46 -0500

    fin

--- server/main.go
+++ server/main.go
@@ -4,14 +4,18 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"os"
 )
 
 func main() {
 	router := mux.NewRouter()
 
 	router.Handle("/", http.FileServer(http.Dir("./static")))
-
-	err := http.ListenAndServe(":8080", router)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	err := http.ListenAndServe(":"+port, router)
 	if err != nil {
 		panic(err)
 	}
